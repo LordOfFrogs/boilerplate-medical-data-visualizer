0a86a7234abc323fcb45fd024723ac238988944b
LordOfFrogs <ndtech260@gmail.com>
2024-07-10 22:05:30 -0400

    completed task

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,57 +4,58 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
-
+df = pd.read_csv('medical_examination.csv')
 # 2
-df['overweight'] = None
+df['overweight'] = df.assign(overweight=(df.weight / (df.height/100)**2) > 25)['overweight'].astype(int)
 
 # 3
-
+df[['cholesterol', 'gluc']] = df[['cholesterol', 'gluc']].map(lambda x: 0 if x == 1 else 1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df, id_vars=['cardio'], value_vars=['cholesterol', 'gluc', 'smoke', 'alco', 'active', 'overweight'])
 
     # 6
-    df_cat = None
-    
+    df_cat= df_cat.value_counts().unstack(level=0).reset_index()
+    df_cat.columns.name = None
+    df_cat = pd.melt(df_cat, id_vars=['variable', 'value'], value_vars=[0, 1], var_name='cardio', value_name='total')
 
     # 7
-
-
-
+    plot = sns.catplot(
+        data=df_cat, x='variable', y='total', hue='value', 
+        col='cardio', kind='bar', errorbar=None
+    )
     # 8
-    fig = None
-
+    fig = plot.figure
 
     # 9
     fig.savefig('catplot.png')
     return fig
 
-
 # 10
 def draw_heat_map():
     # 11
-    df_heat = None
+    df_heat = df[(df['ap_lo']<=df['ap_hi']) &
+                 (df['height'] >= df['height'].quantile(0.025)) &
+                 (df['height'] <= df['height'].quantile(0.975)) &
+                 (df['weight'] >= df['weight'].quantile(0.025)) &
+                 (df['weight'] <= df['weight'].quantile(0.975))]
 
     # 12
-    corr = None
+    corr = df_heat.corr()
 
     # 13
-    mask = None
-
-
+    mask = np.triu(np.ones_like(corr))
 
     # 14
-    fig, ax = None
-
+    fig, ax = plt.subplots(figsize=(8,6))
+    
     # 15
-
-
+    sns.heatmap(
+        corr, ax=ax, mask=mask, annot=True, fmt=".1f", 
+    )
 
     # 16
-    fig.savefig('heatmap.png')
+    fig.savefig('heatmap.png', dpi=100, bbox_inches='tight')
     return fig
